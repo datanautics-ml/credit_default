033af25d899c497e9d3af78b303e5c363aecf579
datanautics-ml <christophe.secouard@gmail.com>
2025-11-11 22:18:34 +0000

    feat: Enhance prediction flow with data and performance drift evaluation with evidently and improved data handling

--- flows/predict_flow.py
+++ flows/predict_flow.py
@@ -19,6 +19,12 @@ from src.config.settings import settings
 from src.models.training import  ModelTrainer
 from src.utils.logging import setup_logging
 import joblib
+from evidently import Report
+from evidently.presets import DataDriftPreset, ClassificationPreset
+from evidently import Dataset
+from evidently import DataDefinition
+from evidently import BinaryClassification
+import datetime as dt
 
 @task(name="Load Data ", retries=1)
 def load_new_data() -> pd.DataFrame:
@@ -38,20 +44,8 @@ def load_new_data() -> pd.DataFrame:
     
     logger.info(f"Loading data from {settings.DATA_DIR}")
     data = pd.read_csv(latest_file_path, header=0)
-    logger.info(f"Data shape: {data.shape}")
-    # Copy the latest file to the processed data directory
-    try:
-        settings.PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
-        dest_path = settings.PROCESSED_DATA_DIR / latest_file
-        if not dest_path.exists():
-            dest_path.write_bytes(latest_file_path.read_bytes())
-            logger.info(f"Copied {latest_file_path} to {dest_path}")
-        else:
-            logger.info(f"Processed file already exists at {dest_path}")
-    except Exception as e:
-        logger.error(f"Failed to copy file to processed dir: {e}")
-        raise
-    return data
+    logger.info(f"Data shape: {data.shape}")   
+    return data, latest_file
 
 @task(name="Load Model")
 def load_model() -> Any:
@@ -87,7 +81,7 @@ def load_model() -> Any:
         raise
 
 @task(name="Make Predictions")
-def make_predictions(model: Any, dataset: pd.DataFrame, scaler: Any = None) -> pd.DataFrame:
+def make_predictions(model: Any, dataset: pd.DataFrame, latest_file: str, scaler: Any = None) -> pd.DataFrame:
     """Make predictions using the loaded model"""
     logger = get_run_logger()
     logger.info("Making predictions")
@@ -101,9 +95,78 @@ def make_predictions(model: Any, dataset: pd.DataFrame, scaler: Any = None) -> p
         X = dataset[features]
         predictions = model.predict(X)
     dataset['predictions'] = predictions
-    
+     
+     # Copy the datset with predictions to the processed data directory
+    try:        
+        settings.PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
+        dest_path = settings.PROCESSED_DATA_DIR / latest_file
+        if not dest_path.exists():
+            dataset.to_csv(dest_path, index=False)
+            logger.info(f"Wrote {latest_file} to {dest_path}")
+            if (settings.PROCESSED_DATA_DIR / "processed_data.csv").exists():
+                processed_data = pd.read_csv(settings.PROCESSED_DATA_DIR / "processed_data.csv")
+                processed_data = pd.concat([processed_data, dataset], ignore_index=True)
+                processed_data.to_csv(settings.PROCESSED_DATA_DIR / "processed_data.csv", index=False)
+            else:
+                dataset.to_csv(settings.PROCESSED_DATA_DIR / "processed_data.csv", index=False)
+            logger.info(f"Updated processed_data.csv in {settings.PROCESSED_DATA_DIR}")
+        else:
+            logger.info(f"Processed file already exists at {dest_path}")
+    except Exception as e:
+        logger.error(f"Failed to copy file to processed dir: {e}")
+        raise
     logger.info("Predictions completed")
     return dataset
+
+@task(name="Evaluate and Monior")
+def evaluate_and_monitor(predictions: pd.DataFrame, reference_path="data/processed/processed_data.csv") -> None:
+    """Generate data and performance drift reports"""
+    logger = get_run_logger()
+    logger.info("Generating data drift and performance reports")
+
+    if Path(reference_path).exists():
+        reference_data = pd.read_csv(reference_path)
+    else:
+        reference_data = predictions
+    # reference_data.rename(columns={"default credit next month": "target"}, inplace=True)
+    # predictions.rename(columns={"default credit next month": "target"}, inplace=True)
+
+    # create evidently definition for binary classification
+    definition = DataDefinition(
+    classification=[BinaryClassification(
+        target="default payment next month",
+        prediction_labels="predictions")],
+        id_column="ID",
+        categorical_columns=["default payment next month", "predictions", 
+                             "SEX","EDUCATION","MARRIAGE"
+        ]
+    )
+    cols = [col for col in predictions.columns if col not in ["MONTH"]]#, "ID"]]
+    # create evidently datasets
+    current_data = Dataset.from_pandas(
+        predictions[cols],
+        data_definition=definition
+    )
+    reference_data = Dataset.from_pandas(
+        reference_data[cols],
+        data_definition=definition
+    )
+
+    
+    
+    # Create evidently reports
+    report = Report(metrics=[
+        DataDriftPreset(),
+        ClassificationPreset()
+    ])
+    # Run the report
+    eval = report.run(reference_data=reference_data, current_data=current_data)
+    timestamp = dt.datetime.now().strftime("%Y%m%d_%H%M")
+    report_path = f"monitoring/reports/drift_report_{timestamp}.html"    
+    eval.save_html(report_path)
+    print(f"Drift report saved: {report_path}")     
+    logger.info("Data drift and performance reports generated")
+    return report_path
 
 @flow(name="ML Model Training Flow")
 def ml_predict_flow(
@@ -119,16 +182,19 @@ def ml_predict_flow(
     logger.info("Starting ML predict flow")
     
     # Load data
-    dataset = load_new_data()
+    dataset, latest_file = load_new_data()
     # Load model
     model, scaler = load_model()
 
     # Make predictions
-    predictions = make_predictions(model=model, dataset=dataset, scaler=scaler)
+    predictions = make_predictions(model=model, dataset=dataset,
+                                   latest_file=latest_file, scaler=scaler)
+    
+    # Evaluate and monitor
+    report_path = evaluate_and_monitor(predictions=predictions)
 
     # Temp: print classification report
-    from sklearn.metrics import classification_report
-    print(classification_report(predictions["default payment next month"], predictions["predictions"]))
+    # print(classification_report(predictions["default payment next month"], predictions["predictions"]))
     
     # Train models
     # training_results = train_ml_models(
